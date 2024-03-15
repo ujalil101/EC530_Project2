30d26fef220210637819f53a2903bebe06d56638
Usman Jalil <ujalil929@gmail.com>
2024-03-15 07:30:24 -0400

    connected mongo, now need to add pdf modules

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from mongo import insert_user_data, verify_credentials
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # checking if username and pwd in db 
+        if verify_credentials(username, password):
+            return redirect(url_for('success'))
+        else:
+            return render_template('login.html', error="Invalid username or password")
+
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        # get data from signup html 
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # checking if passwords are same 
+        if password != confirm_password:
+            return render_template('signup.html', error="Passwords do not match")
+
+        # insert data into mongo module
+        try:
+            insert_user_data(username, password)
+            return redirect(url_for('login'))  # go to success (will redirect later)
+        except Exception as e:
+            return render_template('signup.html', error="Error occurred during signup. Please try again.")
+
     return render_template('signup.html')
+
+@app.route('/success')
+def success():
+    return "Sign up successful!"
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ mongo.py
@@ -0,0 +1,31 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# load password from .env file
+load_dotenv()
+pwd = os.getenv("password")
+
+# connec to mongo
+uri = f"mongodb+srv://ujalil:{pwd}@ec530proj2.hjuujqc.mongodb.net/?retryWrites=true&w=majority&appName=EC530Proj2"
+def connect_to_mongodb():
+    client = MongoClient(uri)
+    db = client['EC530Proj2']
+    return db 
+
+# insert data
+def insert_user_data(username, password):
+    db = connect_to_mongodb()
+    user_data = {
+        'username': username,
+        'password': password,
+        #'email': email,
+        'documents': []  #  an empty list of documents for now.
+    }
+    db.users.insert_one(user_data)
+
+# check if username and password in db for login purposes
+def verify_credentials(username, password):
+    db = connect_to_mongodb()
+    user = db.users.find_one({'username': username, 'password': password})
+    return user is not None
