0ab783d02cd9cf3e54e2a83e89020860ca4b39b4
Usman Jalil <ujalil929@gmail.com>
2024-03-22 00:51:58 -0400

    finsihed nlp module, need to integrate with flask

--- Modules/file_uploader.py
+++ Modules/file_uploader.py
@@ -1,14 +1,20 @@
 import os
 from werkzeug.utils import secure_filename
+from Modules.authorization import connect_to_mongodb
 
 UPLOAD_FOLDER = 'uploads'
 
-def save_pdf(file):
+def save_pdf(file, username):
     if file:
         if not os.path.exists(UPLOAD_FOLDER):
             os.makedirs(UPLOAD_FOLDER)
         filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
+        # Update MongoDB to store the document information
+        db = connect_to_mongodb()
+        db.users.update_one({'username': username}, {'$push': {'documents': {'filename': filename, 'path': file_path}}})
+        
         return True
     else:
         return False
---
+++ Modules/nlp_analysis.py
@@ -0,0 +1,81 @@
+import fitz
+import re
+from collections import Counter
+from textblob import TextBlob
+
+# extract text from pdf
+def extract_text_from_pdf(pdf_file_path):
+    text = ""
+    with fitz.open(pdf_file_path) as doc:
+        for page_num in range(len(doc)):
+            page = doc.load_page(page_num)
+            text += page.get_text()
+    return text
+
+def preprocess_text(text):
+    text = re.sub(r'\s+', ' ', text)
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+    return sentences
+
+# calculate word frequency in sentences
+def calculate_word_frequency(sentence):
+    # convert sentences to words
+    words = re.findall(r'\w+', sentence.lower())
+    # count word freq
+    word_freq = Counter(words)
+    return word_freq
+
+# score sentences based on word frequency
+def score_sentences(sentences):
+    sentence_scores = {}
+    for sentence in sentences:
+        # calc word freq for current sentence
+        word_freq = calculate_word_frequency(sentence)
+        # sum  frequencies of all words in the sentence to score
+        score = sum(word_freq.values())
+        # store sentence score in sentence_scores
+        sentence_scores[sentence] = score
+    return sentence_scores
+
+# generate summary 
+def generate_summary(text, num_sentences=3):
+    # preprocess text and split into sentences
+    sentences = preprocess_text(text)
+    # score sentences based on word freq
+    sentence_scores = score_sentences(sentences)
+    # sort sentences based on their scores 
+    sorted_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)
+    # select top N setnences
+    summary = ' '.join(sorted_sentences[:num_sentences])
+    return summary
+
+# sentiment analysis
+def perform_sentiment_analysis(text):
+    blob = TextBlob(text)
+    # get sentiment polarity score (-1 to 1) from TextBlob
+    sentiment_score = blob.sentiment.polarity
+    # get sentiment label based on the polarity score
+    if sentiment_score > 0:
+        sentiment = "positive"
+    elif sentiment_score < 0:
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+    return sentiment
+
+# analyze analyze uploaded PDF
+def analyze_uploaded_pdf(pdf_file_path):
+    # get text from PDF
+    text = extract_text_from_pdf(pdf_file_path)
+    # generate summary
+    summary = generate_summary(text)
+    # perform sentiment analysis
+    sentiment = perform_sentiment_analysis(text)
+    
+    return summary, sentiment
+
+if __name__ == "__main__":
+    pdf_file_path = ""  
+    summary, keywords, sentiment = analyze_uploaded_pdf(pdf_file_path)
+    print("Summary:", summary)
+    print("Sentiment:", sentiment)
